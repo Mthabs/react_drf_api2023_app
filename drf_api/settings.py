b1f2605d51acc577122fb64dbafaa206a5b1c476
Mthabs <>
2023-11-26 19:41:59 +0000

    add correcting error in the settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -104,11 +104,11 @@ MIDDLEWARE = [
 ]
  if 'CLIENT_ORIGIN' in os.environ:
      CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN')
+        os.environ.get('CLIENT_ORIGIN')
      ]
  else:
      CORS_ALLOWED_ORIGIN_REGEXES = [
-         r"^https://.*\.gitpod\.io$",
+        r"^https://.*\.gitpod\.io$",
      ]
  CORS_ALLOW_CREDENTIALS = True
 
