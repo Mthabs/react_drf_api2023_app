ba4d60e6f0c7d8e614fe2abc6e14900e7d466d09
Mthabs <>
2023-12-02 15:19:12 +0000

    add changes to settings Debug

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -59,7 +59,8 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
+"DEBUG" in os.environ
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'),
 '8000-mthabs-react-drf-api2023-w4yuah359v.us2.codeanyapp.com']
