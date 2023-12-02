d671c89efa63f9c8b815bbd89960fba07c397430
Mthabs <>
2023-12-02 13:00:48 +0000

    add rearranging urlpatterns

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -26,9 +26,9 @@ from followers import views as followers_views
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/logout/', logout_route),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),
