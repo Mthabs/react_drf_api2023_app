aba3aa28537d9340859cb00278d148e04b72a653
Mthabs <mthabs1@gmail.com>
2023-12-04 16:59:23 +0000

    add changes to settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -104,15 +104,20 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-
 if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        os.environ.get('CLIENT_ORIGIN_DEV')
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
+#if 'CLIENT_ORIGIN' in os.environ:
+#   CORS_ALLOWED_ORIGINS = [
+#        os.environ.get('CLIENT_ORIGIN')
+#    ]
+
+#if 'CLIENT_ORIGIN_DEV' in os.environ:
+#    CORS_ALLOWED_ORIGIN_REGEXES = [
+#        os.environ.get('CLIENT_ORIGIN_DEV')
+#    ]
 
 CORS_ALLOW_CREDENTIALS = True
 
@@ -140,16 +145,16 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# if 'DEV' in os.environ:
-#   DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.sqlite3',
-#            'NAME': BASE_DIR / 'db.sqlite3',
-#        }
-#}
-#else:
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+}
+else:
 
-DATABASES = {
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
    
