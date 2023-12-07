99f0b748927a83672de6fe05cc3110547d23d181
Mthabs <mthabs1@gmail.com>
2023-12-07 11:46:12 +0000

    add remiving unnecessary files

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api2023-appl-14278a611b47.gitpod.io/";
+//axios.defaults.baseURL = "https://drf-api2023-appl-14278a611b47.gitpod.io/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
