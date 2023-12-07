128f1a789a7612fe3ff534b5d93594dee5d436db
Mthabs <mthabs1@gmail.com>
2023-12-07 11:38:35 +0000

    add clone react with drf APi

---
+++ frontend/src/api/axiosDefaults.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://drf-api2023-appl-14278a611b47.gitpod.io/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
