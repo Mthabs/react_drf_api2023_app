eaccce724da67fd861f9992294321afe844d631e
Mthabs <mthabs1@gmail.com>
2023-12-09 00:02:19 +0000

    add default images

--- posts/models.py
+++ posts/models.py
@@ -29,7 +29,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../profile_default_imagehome', blank=True
+        upload_to='images/', default='../default_post_wv7bvz', blank=True
     )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'
